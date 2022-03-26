17b35f65f7d5fc833ac7e0c834f54e5e813c2d73
Sara <bauer328@umn.edu>
2022-03-26 14:29:29 -0500

    created quick script to create .csv of filenames.

---
+++ QUICK_list_of_filenames.R
@@ -0,0 +1,6 @@
+#title: "Quick work. Create .csv of filenames"
+#author: "Sara DeLaurentis"
+#date: "03/26/2022"
+#output: html_document
+#purpose: Attach BASE columns of metadata from filenames that will be helpful for analysis. ("site", "rep", "position", "buttonID", "season")
+
